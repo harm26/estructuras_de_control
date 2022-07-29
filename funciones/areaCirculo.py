30e4776ed41231b34700b4f43e4567bf5b55281d
harm26 <harmendoza@gmail.com>
2022-07-29 18:14:53 -0400

    se crea carpeta funciones con files que calculan area de triangulos y circulos

---
+++ funciones/areaCirculo.py
@@ -0,0 +1,9 @@
+import math
+
+
+def areaCirculo(r):
+    return math.pi*r**2
+
+res = areaCirculo(1)
+
+print(res)
---
+++ funciones/areaTriangulo.py
@@ -0,0 +1,6 @@
+def areaTriangulo(base, altura):
+
+    return base*altura/2
+resul = areaTriangulo(2,3)
+
+print(resul)

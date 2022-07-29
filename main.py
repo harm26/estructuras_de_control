6569aa738c187cab0fb043906acfa861515c412d
harm26 <harmendoza@gmail.com>
2022-07-29 13:48:22 -0400

    se crea un programa que le pregunte al usuario su edad y este indique si es o no mayor de edad

---
+++ main.py
@@ -0,0 +1,13 @@
+# Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+
+edad = int(input("Indicanos tu edad por favor: "))
+
+if edad >= 18:
+    print("Eres mayor de edad")
+
+else:
+    print("Eres menor de edad")
+
+
+
